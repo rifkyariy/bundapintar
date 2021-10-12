54b056619c8e05c60f78c524a5781860fea32814
rifkyariy <rifkyariy@gmail.com>
2021-10-12 19:27:22 +0700

    add reference mpasi

--- pages/mpasi/index.vue
+++ pages/mpasi/index.vue
@@ -2,8 +2,8 @@
   <div>
     <Navbar :title="title"/>
     <div>
-      <img src="menu_makanan-" alt="">
       <img v-for="index in 33" :key="index" class="pb-2" :src="'/assets/img/menu_makanan/menu_makanan-'+index+'.jpg'" alt="">
+      <span>Referensi : Sitasari dkk, 2014</span>
     </div>
   </div>
 </template>
